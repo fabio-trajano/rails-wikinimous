f410853adcedf8bc7b4ee74eef89c9b6e643367c
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-02-21 17:23:09 +0000

    Wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Start creating articles...'
+
+10.times {
+  Article.create(title: Faker::Book.title, content: Faker::GreekPhilosophers.quote)
+  puts 'created an article'
+}
+
+puts 'Finished!'
